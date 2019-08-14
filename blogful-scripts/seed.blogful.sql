5748563e3b46afe49395d1c0f3e05615565e2f09
Rachel Oh <rachelyjo@gmail.com>
2019-08-14 13:47:50 -0700

    create blogful database

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (id, title, date_published, content)
+VALUES
+    (1, 'first entry', now() - '21 days'::INTERVAL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+    (2, 'second entry', now() - '20 days'::INTERVAL, 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'),
+    (3, 'third entry', now() - '19 days'::INTERVAL, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
+    (4, 'fourth entry', now() - '17 days'::INTERVAL, 'Magna ac placerat vestibulum lectus mauris ultrices eros. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et.'),
+    (5, 'fifth entry', now() - '16 days'::INTERVAL, 'Sit amet tellus cras adipiscing enim. Blandit cursus risus at ultrices mi tempus. '),
+    (6, 'sixth entry', now() - '15 days'::INTERVAL, 'Nisi vitae suscipit tellus mauris.'),
+    (7, 'seventh entry', now() - '14 days'::INTERVAL, 'Nec ullamcorper sit amet risus nullam eget felis. Volutpat odio facilisis mauris sit amet massa vitae tortor condimentum.'),
+    (8, 'eighth entry', now() - '13 days'::INTERVAL, 'Elementum tempus egestas sed sed risus. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. '),
+    (9, 'nineth entry', now() - '12 days'::INTERVAL, 'Dui faucibus in ornare quam viverra orci sagittis. Eget mauris pharetra et ultrices neque ornare aenean euismod.'),
+    (10, 'tenth entry', now() - '10 days'::INTERVAL, 'Arcu non sodales neque sodales. Vel risus commodo viverra maecenas. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt.'),
+    (11, 'eleventh entry', now() - '9 days'::INTERVAL, 'Sagittis eu volutpat odio facilisis mauris sit. Morbi quis commodo odio aenean sed adipiscing.'),
+    (12, 'twelfth entry', now() - '9 days'::INTERVAL, 'Lectus quam id leo in vitae turpis massa. Nunc sed blandit libero volutpat sed cras ornare.'),
+    (13, 'thirteenth entry', now() - '8 days'::INTERVAL, 'Condimentum vitae sapien pellentesque habitant morbi. Sit amet nulla facilisi morbi.'),
+    (14, 'fourteenth entry', now() - '8 days'::INTERVAL, 'Praesent elementum facilisis leo vel fringilla est ullamcorper. Massa sed elementum tempus egestas sed sed risus pretium quam.'),
+    (15, 'fifteenth entry', now() - '7 days'::INTERVAL, 'Arcu dictum varius duis at consectetur. Eget aliquet nibh praesent tristique magna sit.'),
+    (16, 'sixteenth entry', now() - '6 days'::INTERVAL, 'Adipiscing commodo elit at imperdiet. Commodo nulla facilisi nullam vehicula ipsum.'),
+    (17, 'seventeenth entry', now() - '5 days'::INTERVAL, 'Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Imperdiet dui accumsan sit amet nulla facilisi morbi. Facilisis leo vel fringilla est ullamcorper. At risus viverra adipiscing at. Sed risus ultricies tristique nulla. Volutpat lacus laoreet non curabitur gravida. Vitae purus faucibus ornare suspendisse. Eu feugiat pretium nibh ipsum consequat. Aliquam malesuada bibendum arcu vitae elementum curabitur. Augue ut lectus arcu bibendum at varius vel pharetra vel.'),
+    (18, 'eighteenth entry', now() - '4 days'::INTERVAL, 'Felis imperdiet proin fermentum leo vel. Ultrices in iaculis nunc sed. Urna porttitor rhoncus dolor purus. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim.'),
+    (19, 'nineteenth entry', now() - '3 days'::INTERVAL, 'Eget magna fermentum iaculis eu non diam. Mauris in aliquam sem fringilla ut morbi tincidunt augue.'),
+    (20, 'twentieth entry', now() - '2 days'::INTERVAL, 'Ac turpis egestas integer eget aliquet nibh praesent tristique.')
+;
